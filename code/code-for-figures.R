12eaef873e6863920147c2c8d56c33a918342e7e
Heike Hofmann <hofmann@iastate.edu>
2012-03-27 16:56:55 -0500

    updated figures

--- code/code-for-figures.R
+++ code/code-for-figures.R
@@ -1,3 +1,44 @@
+# lineup for airline
+xx <- seq(0, 360, by=1)
+yy <- rep(0.5, length=length(xx))
+refdf <- data.frame(xx=xx, yy=yy)
+
+ggplot(flights_sample, aes(x = (WindDirDegrees-1 + X)%%360, fill = factor(nf, levels = c(0:8)))) +
+geom_bar(binwidth = 10, position="fill") +
+scale_x_continuous(limits = c(0, 360), breaks=seq(0, 360, by=45), labels=c("N", "NE","E","SE","S","SW","W","NW","N")) +
+coord_polar(start = 0) + geom_line(aes(x=xx, y=yy), size = .5, alpha=0.5, colour = "white", inherit.aes=FALSE, data=refdf) + opts(legend.position="none", plot.margin=unit(c(0,0,0,0), "cm")) +
+seq7rev + labs(x = " ", y = " ") 
+ggsave(file="polar_line.pdf", width=4, height=4, dpi=75)
+
+
+ggplot(flights_sample, aes(x = (WindDirDegrees-1 + X)%%360, fill = factor(nf, levels = c(0:8)))) +
+geom_bar(binwidth = 10, position="fill") +
+scale_x_continuous(limits = c(0, 360), breaks=seq(0, 360, by=45), labels=c("N", "NE","E","SE","S","SW","W","NW","N")) +
+coord_polar(start = 0) + opts(legend.position="none", plot.margin=unit(c(0,0,0,0), "cm")) +
+seq7rev + labs(x = " ", y = " ") 
+ggsave(file="polar_noline.pdf", width=4, height=4, dpi=75)
+
+
+ggplot(flights_sample, aes(x = (WindDirDegrees-1 + X)%%360, fill = factor(nf, levels = c(0:8)))) +
+geom_bar(binwidth = 10, position="fill", alpha=0.8) +
+scale_x_continuous(limits = c(0, 360), breaks=seq(0, 360, by=45), labels=c("N", "NE","E","SE","S","SW","W","NW","N")) + geom_hline(yintercept=0.5, size = .5, alpha=0.5, colour = "white") + opts(legend.position="none", plot.margin=unit(c(0,0,0,0), "cm")) +
+seq7rev + labs(x = " ", y = " ") 
+
+ggsave(file="euclid_line.pdf", width=4, height=4, dpi=75)
+
+
+ggplot(flights_sample, aes(x = (WindDirDegrees-1 + X)%%360, fill = factor(nf, levels = c(0:8)))) +
+geom_bar(binwidth = 10, position="fill", alpha=0.8) +
+scale_x_continuous(limits = c(0, 360), breaks=seq(0, 360, by=45), labels=c("N", "NE","E","SE","S","SW","W","NW","N")) + opts(legend.position="none", plot.margin=unit(c(0,0,0,0), "cm")) +
+seq7rev + labs(x = " ", y = " ") 
+
+ggsave(file="euclid_noline.pdf", width=4, height=4, dpi=75)
+
+
+
+
+
+
 cors <- replicate(1000,
 {
 	x = rexp(100)
